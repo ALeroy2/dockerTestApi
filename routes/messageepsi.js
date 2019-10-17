4430cf803f3d30b8be1e6df22b4f89173bbb5dca
Otterman <leroyalexis7633@gmail.com>
2019-10-17 09:13:42 +0200

    fix route

--- routes/messageepsi.js
+++ routes/messageepsi.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('Hello');
+    res.send('{"messageepsi":"hello"}');
 });
 
 module.exports = router;
